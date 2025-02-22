5ed0c557897e15d8b3d0dc1c2b37f719e707530a
laboratorys <359916450@qq.com>
2025-02-22 11:29:17 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM neosmemo/memos:stable
+
+WORKDIR /usr/local/memos
+
+RUN apk add --no-cache curl tar libc6-compat
+
+COPY backup.sh /usr/local/memos/backup.sh
+RUN chmod +x /usr/local/memos/backup.sh
+
+ENTRYPOINT ["/bin/sh", "-c", "/usr/local/memos/backup.sh && /usr/local/memos/entrypoint.sh /usr/local/memos/memos"]
---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+run_backup(){
+  b_path="download/${BAK_VERSION-"latest"}"
+  if [ "${BAK_VERSION-"latest"}" = "latest" ]; then
+    b_path="latest/download"
+  fi
+  curl -s -L "https://github.com/laboratorys/backup2gh/releases/${b_path}/backup2gh-linux-amd64.tar.gz" -o backup2gh.tar.gz \
+      && tar -xzf backup2gh.tar.gz \
+      && rm backup2gh.tar.gz \
+      && chmod +x backup2gh
+  nohup /usr/local/memos/backup2gh > /dev/null 2>&1 &
+}
+run_backup
+sleep 30
